791d31b1c9db09b4c9fd3d61e9df1555267aa6f9
David H <lifeainteasy@web.de>
2025-11-30 15:29:45 +0100

    Rewrite for UI

--- src/german_bank_csv_to_ledger_cli/core.clj
+++ src/german_bank_csv_to_ledger_cli/core.clj
@@ -29,9 +29,9 @@
      in all other cases return auftraggeber as recipient"
   [entry]
   (cond
-    (= (:Transaction-Type entry) "PayPal Europe S.a.r.l. et Cie S.C.A") (str/replace (:Payment-Details entry) #"^\d+" "") ;delete preceding unique numbers, if paypal
-    (= (:Transaction-Type entry) "ABRECHNUNG KARTE") (:Payment-Details entry)
-    (= (:Transaction-Type entry) "") (:Payment-Details entry)
+    (= (:Beneficiary-Originator entry) "PayPal Europe S.a.r.l. et Cie S.C.A") (str/replace (:Payment-Details entry) #"^\d+" "") ;delete preceding unique numbers, if paypal
+    (= (:Beneficiary-Originator entry) "ABRECHNUNG KARTE") (:Payment-Details entry)
+    (= (:Beneficiary-Originator entry) "") (:Payment-Details entry)
     :else
     (:Beneficiary-Originator entry)))
 
@@ -40,21 +40,28 @@
   [conf_map ledger-entry]
   (assoc
    ledger-entry
-   :account
+   :debit_account
    (get conf_map
         (first
          (filter
           #(str/starts-with? (:payee ledger-entry) %) (keys conf_map))) "Uncategorized")))
 
+(defn determine-debit-or-credit-amount [ledger-entry]
+
+  (if (not= (:debit_amount ledger-entry) "")
+    (:debit_amount ledger-entry)
+    (:credit_amount ledger-entry)))
+
+
 (defn build-entry-for-ledger [entry]
-  (let [date-parts (str/split (:Booking-date entry) #"/")
+  (let [date-parts (str/split (:date entry) #"/")
         year (nth date-parts 2)
         month (nth date-parts 0)
         day (nth date-parts 1)
         formatted-date (str year "/" month "/" day)]
-    (str formatted-date " * " "recipient" "\n"
-         "\t" (:money-category entry) "  " (format-amount-to-euro (invert-string-amount (determine-amount-of-tx entry))) "\n"
-         "\tAssets:Bank:Checking  "    (format-amount-to-euro (determine-amount-of-tx entry))  "\n")))
+    (str formatted-date " * " (:payee entry) "\n"
+         "\t" (:debit_account entry) "  " (format-amount-to-euro (invert-string-amount (determine-debit-or-credit-amount entry))) "\n"
+         "\tAssets:Bank:Checking  "    (format-amount-to-euro (determine-debit-or-credit-amount entry))  "\n")))
 
 
 (defn vector-to-hashmap [transaction]
@@ -81,20 +88,23 @@
 (defn csv-entry-to-ledger [csv-transaction]
 ; copy normal
   (let [ledger-transaction
-        {:Booking-date ""
+        {:date ""
          :booked-sign "*"
          :payee ""
-         :account "Uncategorized"
-         :amount_account ""
-         :asset ""
-         :amount_asset ""}]
+         :credit_account "Uncategorized"
+         :credit_amount ""
+         :debit_account ""
+         :debit_amount ""
+         :currency ""}]
 
     (-> ledger-transaction
-        (assoc :Booking-date (:Booking-date csv-transaction))
+        (assoc :date (:Booking-date csv-transaction))
+        (assoc :debit_account (:money-category csv-transaction))
+        (assoc :debit_amount (:Debit csv-transaction))
+        (assoc :currency (:Currency csv-transaction))
+        (assoc :credit_account "Assets:Bank:Checking")
+        (assoc :credit_amount (:Credit csv-transaction))
         (assoc :payee (determine-recipient csv-transaction)))))
-
-;   (determine-recipient entry)
-;   (determine-money-category conf (determine-recipient entry))
 
 (defn -main
   [args]
@@ -111,19 +121,14 @@
    (butlast)
    ;puts the list of vectors and puts it into the correct hashmap
    (map vector-to-hashmap)
-  ; build 
+
    (map csv-entry-to-ledger)
 
    (map #(determine-money-category myconf/recipient-to-moneycategory %))
-;   (map build-entry-for-ledger)
- ;  (map hashmap list1 list2)
 
-   ;put vectors into map
+   (map build-entry-for-ledger)
 
-
-   ;(map (partial convert-to-ledger-format myconf/recipient-to-moneycategory))
-;  (reduce str)
-;   (take 2)
+   (reduce str)
    (println)))
 ;lein run "/home/dave/Downloads/Transactions_300_8126039_00_20251121_171738.csv" > ./output
 
